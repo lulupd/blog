fc6eaf0ddc614559bf5e139b4a753386f86ce111
lucy pires dias <108961140+lulupd@users.noreply.github.com>
2023-04-09 18:19:14 -0500

    Add check to Navigation

--- components/Navigation.js
+++ components/Navigation.js
@@ -5,39 +5,41 @@ import { getAllPostTags } from '@/lib/tags';
 
 
 export default function Navigation({posts, children}) {
-	const [show, setShow] = useState(false);
-	const tags = getAllPostTags(posts);
-    let togglerElements = [];
-	return (
-		<div className="toggler-container">
-			[<a className="toggler" onClick={() => setShow(!show)}>{show ? '-' : '+'}</a>]
-			Navigation
-			{tags.map(tag => {
-                const splitTags = tag.split("/");
-                if (splitTags.length > 1) {
-                   let togglers = splitTags.map((splitTag, index) => {
-                    if (index < splitTags.length) {
-                        index = index + 1
-                        const togglerElement = (
-                                 show ? 
-                                <Toggler tag={splitTags[splitTags.length - index]} key={splitTags[splitTags.length - index]}>{togglerElements.pop()}</Toggler> :
-                                null
+	if (posts) {
+        const [show, setShow] = useState(false);
+        const tags = getAllPostTags(posts);
+        let togglerElements = [];
+        return (
+            <div className="toggler-container">
+                [<a className="toggler" onClick={() => setShow(!show)}>{show ? '-' : '+'}</a>]
+                Navigation
+                {tags.map(tag => {
+                    const splitTags = tag.split("/");
+                    if (splitTags.length > 1) {
+                    let togglers = splitTags.map((splitTag, index) => {
+                        if (index < splitTags.length) {
+                            index = index + 1
+                            const togglerElement = (
+                                    show ? 
+                                    <Toggler tag={splitTags[splitTags.length - index]} key={splitTags[splitTags.length - index]}>{togglerElements.pop()}</Toggler> :
+                                    null
+                            );
+                                togglerElements.push(togglerElement);
+                                return togglerElement;
+                        }
+                    });
+                    return togglers.pop();
+                    } else {
+                        return (
+                            <div key={tag} className="nested-container">
+                                { show ? 
+                                <Toggler tag={tag}></Toggler> :
+                                null}
+                            </div>
                         );
-                            togglerElements.push(togglerElement);
-                            return togglerElement;
                     }
-                   });
-                   return togglers.pop();
-                } else {
-                    return (
-                        <div key={tag} className="nested-container">
-                            { show ? 
-                            <Toggler tag={tag}></Toggler> :
-                            null}
-                        </div>
-                    );
-                }
-			})}				
-		</div>
-		)
+                })}				
+            </div>
+        )
+    }
 }
